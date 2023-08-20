dc1c9174340669295894461a5e1e5bd20bc2d230
realhsb <realhsb@gmail.com>
2023-08-20 22:49:03 +0900

    ✨[feat]: OpenWeatherMap API 주소

---
+++ hao-weather/Hao-weather/Hao-weather/Network/WeatherAPI.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherAPI.swift
+//  Hao-weather
+//
+//  Created by Subeen on 2023/08/19.
+//
+
+import Foundation
+
+enum WeatherAPI {
+    static let baseUrl: String = "https://api.openweathermap.org/"
+    static let secondUrl: String = "data/2.5/weather"
+}
