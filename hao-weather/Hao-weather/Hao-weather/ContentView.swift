e4adbd4faa96e2b26a1859a9c253d9f8a58174c5
realhsb <realhsb@gmail.com>
2023-08-20 22:50:40 +0900

    ✨[feat]: 메인화면 테스트

--- hao-weather/Hao-weather/Hao-weather/ContentView.swift
+++ hao-weather/Hao-weather/Hao-weather/ContentView.swift
@@ -8,19 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // to call ViewModel
+    
+    // 1. state object (!!!!)
+    // 2. Environment Object
+    // 3. Obserable Object (!!!!)
+//    @StateObject var viewModel: WeatherViewModel = WeatherViewModel()
+    @EnvironmentObject var viewModel: WeatherViewModel
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("현재 서울 날씨는 \(viewModel.weatherModel?.weather.first?.description ?? "None")")
+            
+            
+                
+            
         }
-        .padding()
+        // onAppear vs task
+        // 1. onAppear : 
+        // 2. task : 비동기
+        .task {
+            viewModel.weatherResponse()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+//        @StateObject var viewModel: WeatherViewModel = WeatherViewModel()
         ContentView()
+            .environmentObject(WeatherViewModel())
     }
 }
+
+
+//  배열이 하나라 first로 접근, 배열이 여러 개일 경우 foreach로 전체 돌 거나, .filter 사용
+//            "weather": [
+//                   {
+//                       "id": 800,
+//                       "main": "Clear",
+//                       "description": "맑음",
+//                       "icon": "01d"
+//                   }
+//               ]
